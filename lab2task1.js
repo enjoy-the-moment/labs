ea1ad0133f5d6dd21fe8d5521031675aa1bd73a3
ILLIA H <i22holovkots@gmail.com>
2022-10-24 17:58:15 +0000

    pre v.3

--- lab2task1.js
+++ lab2task1.js
@@ -15,14 +15,21 @@ for (let i = 0; i < fr.length; i++) {
 
 const fr = [1, 2, 3, 4, 5];
 const sc = ['a', 'b', 'c', 'd', 'f'];
-const u = [1, 'b', 2, 'c', 3, 'd', 4, 'f', 5, 'f'];
+const u = [1, 'b', 2, 'c', 3, 'd', 4, 'f', 5, 'a'];
 let j = 0, p = 0;
 
 for (let i = 0; i < fr.length; i++) {
+  
+  if (u[p] === fr[fr.length - 1] && u[p + 1] === sc[0]) {
+    console.log(u[p], "відноситься до", sc[0]);
+  }
   if (u[p] === fr[i] && (u[p + 1] === sc[i] || u[p + 1] === sc[i + 1])) {
     console.log(u[p], "відноситься до", u[p + 1]);
     p += 2;
-  } /* else if (u[p] !== fr[i]) {
+  } /*else if (u[p] === fr[fr.length - 1] && u[p + 1] === sc[0]) {
+    console.log(u[p], "відноситься до", sc[0]);
+    p += 2;
+  }/* else if (u[p] !== fr[i]) {
     console.log('Відображення задано не вірно, елемент з першої множини не має відображення в другій');
   } */
 }
