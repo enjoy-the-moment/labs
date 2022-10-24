ebf729d2666cfa6b2f1c255b076ac794ad24c894
ILLIA H <i22holovkots@gmail.com>
2022-10-24 19:33:08 +0000

    Done task1 and task2. Without input

--- lab2task2.js
+++ lab2task2.js
@@ -1,12 +1,23 @@
 const fr = [1, 2, 3, 4, 5];
 const sc = ['a', 'b', 'c', 'd', 'f'];
 let j = 0;
-// ін’єкція:
-console.log('Ін\'єктивне відображення: ')
+
+// ін’єкція (для неї повинно бути на один ел. в другій множині більше, але не обов'язково):
+console.log('Ін\'єктивне відображення: ');
 for(let i = 0; i < fr.length; i++) {
-    if (fr.indexOf(fr[i]) !== -1 && sc.indexOf(sc[i]) !== -1) {
-        console.log(fr[i], "відноситься до", sc[i]);
+    if (fr.length === sc.length) {
+        if (fr.indexOf(fr[i]) !== -1 && sc.indexOf(sc[i]) !== -1) {
+            console.log(fr[i], "відноситься до", sc[i]);
+        }
+    } else if (fr.length < sc.length) {
+        if (fr.indexOf(fr[i]) !== -1 && sc.indexOf(sc[i + 1]) !== -1) {
+            console.log(fr[i], "відноситься до", sc[i + 1]);
+            j++;
+        }
     }
+}
+if (j > 0) {
+    console.log('Елемент', sc[0], 'не зв\'язаний з іншим елементом з першої множини')
 }
 
 // cюр'єкція:
@@ -18,4 +29,13 @@ for(let s = 0; s < fr.length; s++) {
         console.log(fr[s], "відноситься до", sc[s]);
     }
 }
+
+// Бієктивне відображення:
+console.log('Ін\'єктивне відображення: ')
+for(let i = 0; i < fr.length; i++) {
+    if (fr.indexOf(fr[i]) !== -1 && sc.indexOf(sc[i]) !== -1) {
+        console.log(fr[i], "відноситься до", sc[i]);
+    }
+}
+
 console.log(j);
