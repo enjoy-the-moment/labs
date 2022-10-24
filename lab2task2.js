92fe36a9898ee62d3955b358b23df740731e60f3
ILLIA H <i22holovkots@gmail.com>
2022-10-24 18:58:33 +0000

    surrect done

--- lab2task2.js
+++ lab2task2.js
@@ -1,6 +1,6 @@
 const fr = [1, 2, 3, 4, 5];
 const sc = ['a', 'b', 'c', 'd', 'f'];
-
+let j = 0;
 // ін’єкція:
 console.log('Ін\'єктивне відображення: ')
 for(let i = 0; i < fr.length; i++) {
@@ -11,8 +11,11 @@ for(let i = 0; i < fr.length; i++) {
 
 // cюр'єкція:
 console.log('Сюр\'єктивне відображення: ')
-for(let i = 0; i < fr.length; i++) {
-    if (fr.indexOf(fr[i]) !== -1 && sc.indexOf(sc[i]) !== -1) {
-        console.log(fr[i], "відноситься до", sc[i]);
+for(let s = 0; s < fr.length; s++) {
+    if (fr[s] === fr[fr.length - 1]) {
+        console.log(fr[s], "відноситься до", sc[0]);
+    } else if (fr.indexOf(fr[s]) !== -1 && sc.indexOf(sc[s]) !== -1) {
+        console.log(fr[s], "відноситься до", sc[s]);
     }
 }
+console.log(j);
