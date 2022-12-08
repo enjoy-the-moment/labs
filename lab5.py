ad69ba45ea2fa013b608df1f6724637a0c6fa0a9
ILLIA H <91736828+enjoy-the-moment@users.noreply.github.com>
2022-12-09 00:47:46 +0200

    Create lab5.py

---
+++ lab5.py
@@ -0,0 +1,19 @@
+def colour_vertices(graph):
+
+
+  vertices = sorted((list(graph.keys())))
+  colour_graph = {}
+
+  for vertex in vertices:
+    unused_colours = len(vertices) * [True]
+
+    for neighbor in graph[vertex]:
+      if neighbor in colour_graph:
+        colour = colour_graph[neighbor]
+        unused_colours[colour] = False
+    for colour, unused in enumerate(unused_colours):
+      if unused:
+        colour_graph[vertex] = colour
+        break
+
+  return colour_graph
